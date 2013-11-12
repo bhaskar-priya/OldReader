2d2614eb1d41cbe9a68985f7758692f538eccd8d
bhaskar-priya <bhaskar.priya@outlook.com>
2013-11-12 11:08:38 +0530

    Adding popup to display the memory usage.

---
+++ Old Reader/DebugInfoPopup.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Diagnostics;
+using System.Windows.Threading;
+using Microsoft.Phone.Info;
+
+namespace MemoryLeak.WP8
+{
+    public sealed partial class DebugInfoPopup
+    {
+        public DebugInfoPopup()
+        {
+            InitializeComponent();
+        }
+
+        private readonly DispatcherTimer timer = new DispatcherTimer { Interval = TimeSpan.FromMilliseconds(500) };
+        public void PopupControl_OnOpened(object sender, EventArgs e)
+        {
+            timer.Start();
+            timer.Tick += UpdateMemoryInfo;
+        }
+
+        private void UpdateMemoryInfo(object sender, EventArgs e)
+        {
+            try
+            {
+                GC.Collect();
+                GC.WaitForPendingFinalizers();
+                // keep the popup open
+                PopupControl.IsOpen = true;
+            }
+            catch (Exception ex)
+            {
+                // GC.Collect was throwing exceptions in some rare scenarios
+                Debugger.Break();
+            }
+			double usage = (long)DeviceExtendedProperties.GetValue("ApplicationCurrentMemoryUsage");
+			MemoryBlock.Text = string.Format("Memory: {0:N3} MB", (usage / 1024) / 1024.0);
+        }
+    }
+}
